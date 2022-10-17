014042f1fef3b9858a8fbc97924fa1ded1bf96d2
luhe-li <114192631+luhe-li@users.noreply.github.com>
2022-10-17 14:45:17 -0400

    plot sim results

--- 03analyses/03recalibrationModel/02bayesian/02model_fitting_v1/a1_modelFitting.m
+++ 03analyses/03recalibrationModel/02bayesian/02model_fitting_v1/a1_modelFitting.m
@@ -1,9 +1,7 @@
 % This script fits Bayesian causal inference model to the pre- and post-
-% TOJ data, for each session, each participant
+% TOJ data, for each sesion, each participant
 
 clear all; close all; clc; rng('Shuffle');
-subjID = 1;
-sess = 1;
 
 %% manage paths
 currentDir = pwd;
@@ -12,104 +10,114 @@ addpath(genpath([exptDir '02data'])); % data folder, necessary to run organize_d
 addpath(genpath([exptDir '/03analyses/01analysesFunctions'])); % function folder
 addpath('/Users/luhe/Documents/GitHub/bads') % add bads to path, to be changed later
 
-%% organize data
-data = organize_data(subjID, sess);
+all_sub = 1:10;
+all_ses = 1:9;
 
-%% define model parametersxcv
+for sub = all_sub
+    for  ses = all_ses 
+        
+        %% organize data
+        data(sub, ses)= organize_data(sub, ses);
 
-% set fixed & set-up parameters
-model.expo_num_sim = 1e3; % number of simulation for exposure phase
-model.expo_num_trial = 250; % number of *real* trials in exposure phase
-model.num_runs = 100; %fit the model 100 times, each with a different initialization
-model.num_bin = 100; % num bin to approximate mu_shift
-model.thre_r2 = 0.95; %if R2<0.95, we use KDE
+        %% define model parameters
 
-% define the grid for free parameters
-model.paraID = {'\mu_{pre}','\sigma_{pre}','c_{pre}','\lambda','\sigma_{post}',...
-    'c_{post}','p_{common}','\sigma_{soa}','\sigma_{c1}','\sigma_{c2}','alpha'};
-model.numPara = length(model.paraID);
+        % set fixed & set-up parameters
+        model(sub, ses).expo_num_sim = 1e3; % number of simulation for exposure phase
+        model(sub, ses).expo_num_trial = 250; % number of *real* trials in exposure phase
+        model(sub, ses).num_runs = 100; %fit the model 100 times, each with a different initialization
+        model(sub, ses).num_bin = 100; % num bin to approximate mu_shift
+        model(sub, ses).thre_r2 = 0.95; %if R2<0.95, we use KDE
 
-% hard bounds, the range for LB, UB, larger than soft bounds
-paraH.mu1 = [-0.3, 0.3]; % s
-paraH.sigma1 = [0.01, 0.35]; %s
-paraH.c1 = [0.01, 0.35]; % s
-paraH.lambda = [1e-4, 0.06]; % percentage
-paraH.sigma2 = [0.01, 0.35]; %s
-paraH.c2 = [0.01, 0.35]; % s
-paraH.p_common = [1e-4, 1-1e-4]; % weight
-paraH.sigma_soa  = [0.01, 0.2]; % s
-paraH.sigma_c1 = [1e-4, 0.05]; % s
-paraH.sigma_c2  = [0.2, 3]; % s
-paraH.alpha = [1e-3, 0.1]; % percentage
+        % define the grid for free parameters
+        model(sub, ses).paraID = {'\mu_{pre}','\sigma_{pre}','c_{pre}','\lambda','\sigma_{post}',...
+            'c_{post}','p_{common}','\sigma_{soa}','\sigma_{c1}','\sigma_{c2}','alpha'};
+        model(sub, ses).numPara = length(model(sub, ses).paraID);
 
-% soft bounds, the range for PLB, PUB
-paraS.mu1 = [-0.25, 0.25]; % s
-paraS.sigma1 = [0.01, 0.3]; %s
-paraS.c1 = [0.01, 0.3]; % s
-paraS.lambda = [1e-4, 0.06]; % percentage
-paraS.sigma2 = [0.01, 0.3]; %s
-paraS.c2 = [0.01, 0.3]; % s
-paraS.p_common = [0.2, 0.5]; % weight
-paraS.sigma_soa  = [0.01, 0.18]; % s
-paraS.sigma_c1 = [1e-4, 0.03]; % s
-paraS.sigma_c2  = [0.5, 2]; % s
-paraS.alpha = [1e-3, 0.01]; % percentage
+        % hard bounds, the range for LB, UB, larger than soft bounds
+        paraH.mu1 = [-0.3, 0.3]; % s
+        paraH.sigma1 = [0.01, 0.35]; %s
+        paraH.c1 = [0.01, 0.35]; % s
+        paraH.lambda = [1e-4, 0.06]; % percentage
+        paraH.sigma2 = [0.01, 0.35]; %s
+        paraH.c2 = [0.01, 0.35]; % s
+        paraH.p_common = [1e-4, 1-1e-4]; % weight
+        paraH.sigma_soa  = [0.01, 0.2]; % s
+        paraH.sigma_c1 = [1e-4, 0.05]; % s
+        paraH.sigma_c2  = [0.2, 3]; % s
+        paraH.alpha = [1e-3, 0.1]; % percentage
 
-% reorganize parameter bounds to feed to bads
-fn = fieldnames(paraH);
-for k = 1:numel(fn)
-    model.lb(:,k) = paraH.(fn{k})(1);
-    model.ub(:,k) = paraH.(fn{k})(2);
-    model.plb(:,k) = paraS.(fn{k})(1);
-    model.pub(:,k) = paraS.(fn{k})(2);
-end
-model.paraS = paraS; model.paraH = paraH;
+        % soft bounds, the range for PLB, PUB
+        paraS.mu1 = [-0.25, 0.25]; % s
+        paraS.sigma1 = [0.01, 0.3]; %s
+        paraS.c1 = [0.01, 0.3]; % s
+        paraS.lambda = [1e-4, 0.06]; % percentage
+        paraS.sigma2 = [0.01, 0.3]; %s
+        paraS.c2 = [0.01, 0.3]; % s
+        paraS.p_common = [0.2, 0.5]; % weight
+        paraS.sigma_soa  = [0.01, 0.18]; % s
+        paraS.sigma_c1 = [1e-4, 0.03]; % s
+        paraS.sigma_c2  = [0.5, 2]; % s
+        paraS.alpha = [1e-3, 0.01]; % percentage
 
-% set barrier function if needeed
-% model.nonbcon
+        % reorganize parameter bounds to feed to bads
+        fn = fieldnames(paraH);
+        for k = 1:numel(fn)
+            model(sub, ses).lb(:,k) = paraH.(fn{k})(1);
+            model(sub, ses).ub(:,k) = paraH.(fn{k})(2);
+            model(sub, ses).plb(:,k) = paraS.(fn{k})(1);
+            model(sub, ses).pub(:,k) = paraS.(fn{k})(2);
+        end
+        model(sub, ses).paraS = paraS; model(sub, ses).paraH = paraH;
 
-% set OPTIONS to tell bads that my objective function is noisy
-OPTIONS.UncertaintyHandling = 1;
+        % set barrier function if needeed
+        % model(sub, ses).nonbcon
 
-%% run bads for optimization
-
-% define function calculating nll
-funcNLL = @(p) cal_nLL_CI(p(1), p(2), p(3), p(4), p(5), p(6), p(7), ...
-    p(8), p(9), p(10), p(11), model, data);
-
-% get grid initializations
-numREachInit    = 1;
-numSections     = 3;
-model.init      = gridInitializations_11d(model.plb, model.pub, numSections, ...
-    model.num_runs, numREachInit);
-
-% %% plug in parameters to test NLL function
-% for i = 1:10
-%     p = model.init(i,:);
-%     testNLL(i) = cal_nLL_CI(p(1), p(2), p(3), p(4), p(5), p(6), p(7), ...
-%         p(8), p(9), p(10), p(11), model, data);
-% end
-
-%initialize matrices that store negative log likelihood and best-fit paramters
-minNLL          = NaN(1, model.num_runs);
-estimatedP      = NaN(model.num_runs, length(model.lb));
+        % set OPTIONS to tell bads that my objective function is noisy
+        OPTIONS.UncertaintyHandling = 1;
 
 
-parfor i = 1:model.num_runs
-    disp(i);
-    try
-        [estimatedP(i,:),minNLL(i)] = bads(funcNLL, model.init(i,:), model.lb,...
-            model.ub, model.plb, model.pub, [], OPTIONS);
-        disp(estimatedP(i,:));
-        disp(round(minNLL(i),4));
-    catch e
-        disp('Error!')
+
+        %% run bads for optimization
+
+        % define function calculating nll
+        funcNLL = @(p) cal_nLL_CI(p(1), p(2), p(3), p(4), p(5), p(6), p(7), ...
+            p(8), p(9), p(10), p(11), model(sub, ses), data(sub, ses));
+
+        % get grid initializations
+        numREachInit    = 1;
+        numSections     = 3;
+        model(sub, ses).init      = gridInitializations_11d(model(sub, ses).plb, model(sub, ses).pub, numSections, ...
+            model(sub, ses).num_runs, numREachInit);
+
+        % %% plug in parameters to test NLL function
+        % for i = 1:10
+        %     p = model.init(i,:);
+        %     testNLL(i) = cal_nLL_CI(p(1), p(2), p(3), p(4), p(5), p(6), p(7), ...
+        %         p(8), p(9), p(10), p(11), model, data);
+        % end
+
+        %initialize matrices that store negative log likelihood and best-fit paramters
+        minNLL          = NaN(1, model(sub, ses).num_runs);
+        estimatedP      = NaN(model(sub, ses).num_runs, length(model(sub, ses).lb));
+
+        parfor i = 1:model(sub, ses).num_runs
+            disp(i);
+            try
+                [estimatedP(i,:),minNLL(i)] = bads(funcNLL, model(sub, ses).init(i,:), model(sub, ses).lb,...
+                    model(sub, ses).ub, model(sub, ses).plb, model(sub, ses).pub, [], OPTIONS);
+                disp(estimatedP(i,:));
+                disp(round(minNLL(i),4));
+            catch e
+                disp('Error!')
+            end
+        end
+
+        model(sub, ses).estimatedP = estimatedP;
+        model(sub, ses).minNLL     = minNLL;
+
+        %% save the data
+        save("a1_modelFittingResults",'data','model')
+        fprintf('sub %.0f sesion %.0f saved', sub, ses)
     end
 end
 
-
-model.estimatedP = estimatedP;
-model.minNLL     = minNLL;
-
-%% save the data
-save("a1_modelFittingResults",'data','model')
--- 03analyses/03recalibrationModel/02bayesian/02model_fitting_v1/a2_plotModelPred.m
+++ 03analyses/03recalibrationModel/02bayesian/02model_fitting_v1/a2_plotModelPred.m
@@ -2,97 +2,119 @@
 % the model prediction to check whether the best-fitting parameters are
 % reasonable
 
+clear all; clc;
+
 load("a1_modelFittingResults.mat")
 
-%% check distribution of fitted parameters
-figure; hold on
-set(gcf, 'Position', get(0, 'Screensize'));
-set(gca, 'LineWidth', 1, 'FontSize', 15)
-for i = 1:model.numPara
-    subplot(3,4,i)
-    histogram(model.estimatedP(:,i), 10)
-    title(model.paraID{i})
+% %% check distribution of fitted parameters
+% figure; hold on
+% set(gcf, 'Position', get(0, 'Screensize'));
+% set(gca, 'LineWidth', 1, 'FontSize', 15)
+% for i = 1:model.numPara
+%     subplot(3,4,i)
+%     histogram(model.estimatedP(:,i), 10)
+%     title(model.paraID{i})
+% end
+
+currentDir = pwd;
+outDir = [currentDir '/a2_figures'];
+all_sub = 1:10;
+all_ses = 1:9;
+
+for sub = all_sub
+
+    figure; set(gcf, 'Position', get(0, 'Screensize'));
+
+    for  ses = all_ses
+
+        %% extract the best-fitting parameters based on minNLL
+        %find the index that corresponds to the minimum nLL
+        [fits.min_val, fits.min_idx] = min(model(sub, ses).minNLL);
+        %find the corresponding estimated parameters
+        fits.p = model(sub, ses).estimatedP(fits.min_idx,:);
+        p = fits.p;
+
+        %% simulate mu_shift to obtain mu_post
+        mu_shift = NaN(1, model(sub, ses).expo_num_sim);
+        for t   = 1:model(sub, ses).expo_num_sim
+            mu_shift(t)   = simulate_mu_shift_MA(model(sub, ses).expo_num_trial, ...
+                data(sub, ses).adaptor_soa, fits.p(1),...
+                fits.p(7), fits.p(8), fits.p(9), fits.p(10), fits.p(11));
+        end
+
+        % mu_post = mu_pre + simulated mean(mu_shift)
+        mu2 = fits.p(1) + mean(mu_shift);
+
+        %% plot PMF predictions
+
+        %m ake a finer grid for the timing difference between the auditory and the
+        %visual stimulus
+        SOA_finer  = linspace(data(sub, ses).pre_s_unique(1), data(sub, ses).pre_s_unique(end), 1000);
+
+        % define psychometric function (PMF)
+        P_Afirst = @(SOA, mu, sig, c, lambda) lambda/3 + (1-lambda).*normcdf(-c, SOA - mu, sig);
+        P_Vfirst = @(SOA, mu, sig, c, lambda) lambda/3 + (1-lambda).*(1 - normcdf(c, SOA-mu, sig));
+        P_simultaneous = @(SOA, mu, sig, c, lambda) ...
+            1 - (lambda/3 + (1-lambda).*normcdf(-c, SOA - mu, sig)) ...
+            - (lambda/3 + (1-lambda).*(1 - normcdf(c, SOA-mu, sig)));
+
+        % fit PMF with best-fitting parameters
+        pmf = {P_Vfirst(SOA_finer, p(1), p(2), p(3), p(4)), ...
+            P_simultaneous(SOA_finer, p(1), p(2), p(3), p(4)), ...
+            P_Afirst(SOA_finer, p(1), p(2), p(3), p(4)), ...
+            P_Vfirst(SOA_finer, mu2, p(5), p(6), p(4)), ...
+            P_simultaneous(SOA_finer, mu2, p(5), p(6), p(4)), ...
+            P_Afirst(SOA_finer, mu2, p(5), p(6), p(4))};
+
+        % set plotting parameters
+        cmp  = [229, 158, 168; 203, 227, 172; 171,223,235;...
+            216, 49, 91; 175, 213, 128; 88,193,238]./255;
+        alpha_value  = [repmat(0.6, 1, 3), repmat(0.6, 1, 3)];
+        line_pattern   = {'--','--','--','-','-','-'};
+
+        % decide jitter direction by adaptor soa
+        if data(sub, ses).adaptor_soa <= 0
+            jitter  = 0.01;
+        else
+            jitter = -0.01;
+        end
+
+        % initiate figure
+        subplot(3,3,ses); hold on
+        set(gca, 'LineWidth', 2, 'FontSize', 20)
+
+        % plot raw data
+        for i = 1:3
+            dots(i) = plot(data(sub, ses).pre_s_unique + jitter, data(sub, ses).pre_pResp(i,:), 'o',...
+                'MarkerSize',7,'MarkerFaceColor', 'none','MarkerEdgeColor',cmp(i+3,:),'LineWidth',1.2);
+            dots(i+3) = plot(data(sub, ses).pre_s_unique - jitter, data(sub, ses).post_pResp(i,:), 'o',...
+                'MarkerSize',7,'MarkerFaceColor', cmp(i+3,:),'MarkerEdgeColor',cmp(i+3,:),'LineWidth',1.2);
+        end
+
+        % plot pmf (without error bars)
+        for i  = [4:6, 1:3] % lighter lines (pretest) are at the front
+            lines(i)  = plot(SOA_finer, pmf{i}, line_pattern{i},'Color', cmp(i,:), 'LineWidth', 2);
+        end
+
+        % plot mu
+        h1 = xline(p(1),':','LineWidth',2,'Color', repmat(0.3, 1, 3));
+        h2 = xline(mu2,'LineWidth',2,'Color', repmat(0.3, 1, 3));
+
+        % look better
+        tick_soa = [-500, -300:100:300,500]./1000;
+        tick_p = [0:0.25:1];
+        xticks(tick_soa)
+        xticklabels(strsplit(num2str(tick_soa)))
+        yticks(tick_p)
+        yticklabels(strsplit(num2str(tick_p)))
+        xlabel('SOA (ms)')
+        ylabel('proportion of responses')
+        title(['adaptor SOA = ' num2str(data(sub, ses).adaptor_soa)])
+        
+    end
+    
+    % save figure
+    flnm = ['sub' num2str(sub) 'ses' num2str(ses)];
+    saveas(gca, fullfile(outDir, flnm),'epsc')
+
 end
-
-%% extract the best-fitting parameters based on minNLL
-%find the index that corresponds to the minimum nLL
-[fits.min_val, fits.min_idx] = min(model.minNLL);
-%find the corresponding estimated parameters
-fits.p = model.estimatedP(fits.min_idx,:);
-p = fits.p;
-
-%% simulate mu_shift to obtain mu_post
-mu_shift = NaN(1, model.expo_num_sim);
-for t   = 1:model.expo_num_sim
-    mu_shift(t)   = simulate_mu_shift_MA(model.expo_num_trial, data.adaptor_soa, fits.p(1),...
-        fits.p(7), fits.p(8), fits.p(9), fits.p(10), fits.p(11));
-end
-
-% mu_post = mu_pre + simulated mean(mu_shift)
-mu2 = fits.p(1) + mean(mu_shift);
-
-%% plot PMF predictions
-
-%m ake a finer grid for the timing difference between the auditory and the
-%visual stimulus
-SOA_finer  = linspace(data.pre_s_unique(1), data.pre_s_unique(end), 1000);
-
-% define psychometric function (PMF)
-P_Afirst = @(SOA, mu, sig, c, lambda) lambda/3 + (1-lambda).*normcdf(-c, SOA - mu, sig);
-P_Vfirst = @(SOA, mu, sig, c, lambda) lambda/3 + (1-lambda).*(1 - normcdf(c, SOA-mu, sig));
-P_simultaneous = @(SOA, mu, sig, c, lambda) ...
-    1 - (lambda/3 + (1-lambda).*normcdf(-c, SOA - mu, sig)) ...
-    - (lambda/3 + (1-lambda).*(1 - normcdf(c, SOA-mu, sig)));
-
-% fit PMF with best-fitting parameters
-pmf = {P_Vfirst(SOA_finer, p(1), p(2), p(3), p(4)), ...
-    P_simultaneous(SOA_finer, p(1), p(2), p(3), p(4)), ...
-    P_Afirst(SOA_finer, p(1), p(2), p(3), p(4)), ...
-    P_Vfirst(SOA_finer, mu2, p(5), p(6), p(4)), ...
-    P_simultaneous(SOA_finer, mu2, p(5), p(6), p(4)), ...
-    P_Afirst(SOA_finer, mu2, p(5), p(6), p(4))};
-
-% set plotting parameters
-cmp  = [229, 158, 168; 203, 227, 172; 171,223,235;...
-    216, 49, 91; 175, 213, 128; 88,193,238]./255;
-alpha_value  = [repmat(0.6, 1, 3), repmat(0.6, 1, 3)];
-line_pattern   = {'--','--','--','-','-','-'};
-
-% decide jitter direction by adaptor soa
-if data.adaptor_soa <= 0
-    jitter  = 0.01;
-else
-    jitter = -0.01;
-end
-
-% initiate figure
-figure; hold on
-set(gca, 'LineWidth', 2, 'FontSize', 20)
-set(gcf, 'Position',[10 10 700 400])
-
-% plot raw data
-for i = 1:3
-    dots(i) = plot(data.pre_s_unique + jitter, data.pre_pResp(i,:), 'o',...
-        'MarkerSize',7,'MarkerFaceColor', 'none','MarkerEdgeColor',cmp(i+3,:),'LineWidth',1.2);
-    dots(i+3) = plot(data.pre_s_unique - jitter, data.post_pResp(i,:), 'o',...
-        'MarkerSize',7,'MarkerFaceColor', cmp(i+3,:),'MarkerEdgeColor',cmp(i+3,:),'LineWidth',1.2);
-end
-
-% plot pmf (without error bars)
-for i  = [4:6, 1:3] % lighter lines (pretest) are at the front
-    lines(i)  = plot(SOA_finer, pmf{i}, line_pattern{i},'Color', cmp(i,:), 'LineWidth', 2);
-end
-
-% plot mu
-h1 = xline(p(1),':','LineWidth',2,'Color', repmat(0.3, 1, 3));
-h2 = xline(mu2,'LineWidth',2,'Color', repmat(0.3, 1, 3));
-
-% look better
-tick_soa = [-500, -300:100:300,500]./1000;
-tick_p = [0:0.25:1];
-xticks(tick_soa)
-xticklabels(strsplit(num2str(tick_soa)))
-yticks(tick_p)
-yticklabels(strsplit(num2str(tick_p)))
-xlabel('SOA (ms)')
-ylabel('proportion of responses')
