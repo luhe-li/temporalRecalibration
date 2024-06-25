220d56e8abc4e7ad869c5f53663d166156cce5e3
luhe-li <ll3981@nyu.edu>
2024-06-25 13:24:34 -0400

    add bads

---
+++ review1_models/atheoretical_models/gauss_shiftC/runMatlab.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#SBATCH --job-name=FitGaussShiftC
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=32GB
+#SBATCH --time=20:00:00
+#SBATCH --mail-type=END
+#SBATCH --mail-user=ll3981@nyu.edu
+#SBATCH --output=slurm%j.out
+
+module purge
+module load matlab/2022a
+
+matlab <<EOF
+
+model_fit
+
+EOF
